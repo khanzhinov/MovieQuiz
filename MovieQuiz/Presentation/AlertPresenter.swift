34093411eba62d7f3d72bc82d9543d295cddab36
Pavel <khanzhinov@yandex.ru>
2023-06-15 15:49:28 +0300

    запускается , но работает криво

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -7,24 +7,24 @@
 
 import UIKit
 
- struct AlertPresenter {
-     weak var delegate: AlertPresenterDelegate?
-
-     func present(alert: AlertModel) {
-
-         let alertController = UIAlertController(
-             title: alert.title,
-             message: alert.message,
-             preferredStyle: .alert)
-
-         let action = UIAlertAction(
-             title: alert.buttonText,
-             style: .default,
-             handler: alert.completion)
-         alertController.addAction(action)
-         delegate?.didPresentAlert(alert: alertController)
-     }
- }
+struct AlertPresenter {
+    weak var delegate: AlertPresenterDelegate?
+    
+    func present(alert: AlertModel) {
         
+        let alertController = UIAlertController(
+            title: alert.title,
+            message: alert.message,
+            preferredStyle: .alert)
         
-        
+        let action = UIAlertAction(
+            title: alert.buttonText,
+            style: .default,
+            handler: alert.completion)
+        alertController.addAction(action)
+        delegate?.didPresentAlert(alert: alertController)
+    }
+}
+
+
+
--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -7,6 +7,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     @IBOutlet weak private var countLabel: UILabel!
     @IBOutlet weak private var imageView: UIImageView!
     @IBOutlet weak private var textLabel: UITextView!
+    @IBOutlet weak private var activityIndicator: UIActivityIndicatorView!
     
     private let questionsAmount: Int = 10
     private var questionFactory: QuestionFactoryProtocol?
@@ -44,34 +45,13 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        imageView.layer.cornerRadius = 20
-        
-        alertPresenter = AlertPresenter()
-        alertPresenter?.delegate = self
-        questionFactory = QuestionFactory(delegate: self)
-        questionFactory?.requestNextQuestion()
+       
+       imageView.layer.cornerRadius = 20
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader()) //delegate: self)
         statisticService = StatisticServiceImplementation()
-        
-        let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let fileURL = documentDirectory.appendingPathComponent("top250MoviesIMDB.json")
-        var jsonString = ""
-        
-        do {
-            jsonString = try String(contentsOf: fileURL)
-        } catch FileManagerError.fileDoesntExist {
-            print("Файл по адресу \(fileURL) не существует")
-        } catch {
-            print("Неизвестная ошибка чтения из файла \(fileURL)")
-        }
-        
-        let data = jsonString.data(using: .utf8)!
-        
-        do {
-            _ = try JSONDecoder().decode(Top.self, from: data)
-        } catch {
-            print("Failed to parse \(jsonString)")
-        }
+
+        showLoadingIndicator()
+        questionFactory?.loadData()
     }
     
     // MARK: - QuestionFactoryDelegate
@@ -161,11 +141,55 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate, 
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
-            image: UIImage(named: model.image) ?? UIImage(),
+            image: UIImage(data: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-        
     }
     
+    private func showLoadingIndicator() {
+        activityIndicator.isHidden = false // говорим, что индикатор загрузки не скрыт
+        activityIndicator.startAnimating() // включаем анимацию
+    }
+    
+
+         private func hideLoadingIndicator() {
+             activityIndicator.isHidden = true
+         }
+    
+    private func showNetworkError(message: String) {
+                 hideLoadingIndicator()
+
+                 let alertError = UIAlertController(
+                     title: "Что-то пошло не так(",
+                     message: message,
+                     preferredStyle: .alert)
+                 let action = UIAlertAction(title: "Попробовать еще раз?",
+                                            style: .default) { _ in
+
+
+                     self.questionFactory = QuestionFactory(moviesLoader: MoviesLoader())
+
+                     self.questionFactory?.delegate = self
+
+                     self.statisticService = StatisticServiceImplementation()
+
+                     self.questionFactory?.loadData()
+
+                     self.showLoadingIndicator()
+
+                 }
+                 alertError.addAction(action)
+                 self.present(alertError, animated: true, completion: nil)
+             }
+     
+    
+    func didFailToLoadData(with error: Error) {
+        showNetworkError(message: error.localizedDescription) // возьмём в качестве сообщения описание ошибки
+    }
+    
+    func didLoadDataFromServer() {
+        activityIndicator.isHidden = true // скрываем индикатор загрузки
+        questionFactory?.requestNextQuestion()
+    }
 }
 
